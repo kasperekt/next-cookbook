b71c5960068f158c655519802169dd69aa27f0f7
Tomasz Kasperek <kasperekt@gmail.com>
2020-05-01 11:09:50 +0200

    Auth0 Log process.env

--- src/utils/auth0.ts
+++ src/utils/auth0.ts
@@ -1,5 +1,7 @@
 import { initAuth0 } from '@auth0/nextjs-auth0'
 
+console.log('Env', process.env)
+
 export default initAuth0({
   domain: process.env.AUTH0_DOMAIN!,
   clientId: process.env.AUTH0_CLIENT_ID!,
