a870d1f9da5c44cb8d2efc6c665429d982eece2d
Tomasz Kasperek <kasperekt@gmail.com>
2020-05-04 08:38:00 +0200

    Debug

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import '../styles/main.scss'
 import Layout from '../layout/Layout'
 
+console.log('process.env', process.env)
+
 export default function MyApp({ Component, pageProps }: any) {
   return (
     <Layout>
