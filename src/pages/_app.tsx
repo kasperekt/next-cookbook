5666e62445b1fb6d3783ebc4aebe3e565d45de97
Tomasz Kasperek <kasperekt@gmail.com>
2020-05-04 08:53:00 +0200

    Revert "Debug"
    This reverts commit a870d1f9da5c44cb8d2efc6c665429d982eece2d.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,6 @@
 import '../styles/main.scss'
 import Layout from '../layout/Layout'
 
-console.log('process.env', process.env)
-
 export default function MyApp({ Component, pageProps }: any) {
   return (
     <Layout>
